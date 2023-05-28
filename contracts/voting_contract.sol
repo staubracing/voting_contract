baafda2500c5415f96a1f94ba4c269adad082adf
StaubRacing <staubracing@gmail.com>
2023-05-28 08:19:18 -0500

    first Commit

---
+++ contracts/voting_contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+
+/// @title Voting with delegation.
+/// @author Staub
+/// @notice We have three types of participants: voters, delegates, and candidates
+/// @notice Voters can delegate their vote to a delegate
+/// @notice Delegates can vote on behalf of others
+/// @notice Candidates are the options in the voting contract
+
+contract Voting {
+    
+
+}
+
