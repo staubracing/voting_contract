0d30d11dd75f78cdce92e1e7ee3c03e1b57042c5
StaubRacing <staubracing@gmail.com>
2023-06-01 06:06:28 -0500

    added: structs for voter and proposal

--- contracts/voting_contract.sol
+++ contracts/voting_contract.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-
-
 /// @title Voting with delegation.
 /// @author Staub
 /// @notice We have three types of participants: voters, delegates, and candidates
@@ -12,7 +10,16 @@ pragma solidity ^0.8.0;
 /// @notice Candidates are the options in the voting contract
 
 contract Voting {
-    
-
-}
-
+    /// @notice This declares a state variable that stores a `Voter` struct for each possible address.
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
+    /// @notice This is a type for a single voter.
+    struct Voter {
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+} // end contract Voting
