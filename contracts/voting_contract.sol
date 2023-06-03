3cc506127b42207f1f7140c2379e86e0e050ff78
StaubRacing <staubracing@gmail.com>
2023-06-03 08:00:19 -0500

    Added: constructor for proposal names, and array push

--- contracts/voting_contract.sol
+++ contracts/voting_contract.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.0;
 /// @notice Candidates are the options in the voting contract
 
 contract Voting {
-    /// @notice This declares a state variable that stores a `Voter` struct for each possible address.
+    /// @notice This the type for a single proposal.
     struct Proposal {
         bytes32 name;
         uint voteCount;
@@ -22,4 +22,42 @@ contract Voting {
         address delegate;
         uint vote;
     }
+    /// @notice This is a type for the chairperson of the voting contract.
+    /// @notice The chairperson can add and remove voters and candidates.
+    /// @notice The chairperson can also end the voting process.
+    /// @notice The chairperson is the owner of the voting contract.
+    address public chairperson;
+
+    /// @notice This declares a state variable that stores a `Voter` struct for each possible address.
+    mapping(address => Voter) public voters;
+
+    /// @notice This is a dynamically-sized array of `Proposal` structs.
+    Proposal[] public proposals;
+
+    /// @notice This creates a new ballot to choose one of `proposalNames`.
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        /// @notice For each of the provided proposal names, create a new proposal object and add it to the end of the array.
+        for (uint i = 0; i < proposalNames.length; i++) {
+            /// @notice `Proposal({...})` creates a temporary Proposal object and `proposals.push(...)`
+            /// @notice appends it to the end of `proposals`.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+
+        
+
+
+
+
+
+
+
+
+    
 } // end contract Voting
