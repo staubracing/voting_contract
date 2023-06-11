d19374f801db793fa655373252bb9e0ce28b2b64
StaubRacing <staubracing@gmail.com>
2023-06-10 21:17:19 -0500

    added: delegate function_needs work_

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -17,10 +17,10 @@ contract Voting {
     }
     /// @notice This is a type for a single voter.
     struct Voter {
-        uint8 weight;
-        bool voted;
-        address delegate;
-        uint vote;
+        uint8 weight; // weight is accumulated by delegation
+        bool voted; // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote; // index of the voted proposal
     }
     /// @notice This is a type for the chairperson of the voting contract.
     /// @notice The chairperson can add and remove voters and candidates.
@@ -55,10 +55,41 @@ contract Voting {
         /// @notice This used to consume all gas in old EVM versions, but not anymore.
         /// @notice It is often a good idea to use `require` to check if functions are called correctly.
         /// @notice As a second argument, you can also provide an explanation about what went wrong.
-        require(msg.sender == chairperson, "Only chairperson can give right to vote.");
-        require(!voters[voter].voted, "The voter already voted.");
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
+        require(msg.sender == chairperson, "Only chairperson can give right to vote."); // only chairperson can give right to vote
+        require(!voters[voter].voted, "The voter already voted.");  // weight is 0 by default for existing voters
+        require(voters[voter].weight == 0); // weight is 0 by default for existing voters
+        voters[voter].weight = 1; // weight is 1 by default for new voters (weight is 0 by default for existing voters)
     } // end function giveRightToVote
 
-} // end contract Voting
+
+    /// @notice Delegate your vote to the voter `to`.
+    function delegate(address to) external {
+        Voter storage sender = voters[msg.sender]; // assigns reference
+        require(sender.weight != 0, "Has no right to vote."); // check if sender has right to vote
+        require(!sender.voted, "Already voted."); // check if sender already voted
+
+        require (to != msg.sender, "Self-delegation is disallowed."); // check if sender is not delegating to himself        
+
+        while (voters[to].delegate != address(0)) { // check if to is delegating to someone else
+            to = voters[to].delegate; // to is now the delegate of the delegate
+            require(to != msg.sender, "Found loop in delegation."); // check if there is a loop in delegation
+        }
+
+        voter storage delegate_ = voters[to]; // assigns reference to delegate of sender (to)
+
+        require(delegate_.weight >= 1); // check if delegate has right to vote
+        
+        sender.voted = true; // sender voted
+        sender.delegate = to; // sender delegated to to
+
+        if (delegate_.voted) { // check if delegate already voted
+            proposals[delegate_.vote].voteCount += sender.weight; // add weight to proposal
+        } else {
+            delegate_.weight += sender.weight; // add weight to delegate
+        }
+    } // end function delegate
+    
+   }
+
+
+
